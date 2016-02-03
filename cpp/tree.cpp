eae5ea4d51cf1e4d14b358b6198fc108eacf2697
Jeeyoung Kim <jee@squareup.com>
2016-02-03 00:27:54 -0800

    code reuse :)

--- cpp/tree.cpp
+++ cpp/tree.cpp
@@ -28,133 +28,86 @@ public:
 template <typename K, typename V> class Tree {
   unique_ptr<Node<K, V>> root;
   const V EMPTY;
+  // find a reference to the pointer that is either:
+  // - contains a node with the given key.
+  // - unassigned pointer that can contain the given key.
+  const unique_ptr<Node<K, V>> &find(const K &key) const {
+    const unique_ptr<Node<K, V>> *ptr = &root;
+    while (*ptr) {
+      const Node<K, V> &node = *(*ptr);
+      if (node.key > key) {
+        if (node.left) {
+          ptr = &(node.left);
+        } else {
+          return node.left;
+        }
+      } else if (node.key < key) {
+        if (node.right) {
+          ptr = &(node.right);
+        } else {
+          return node.right;
+        }
+      } else {
+        return *ptr;
+      }
+    }
+    return root;
+  }
+
+  unique_ptr<Node<K, V>> &find(const K &key) {
+    return const_cast<unique_ptr<Node<K, V>> &>(
+        static_cast<const Tree<K, V> &>(*this).find(key));
+  }
 
 public:
   void put(const K &key, const V &value) {
-    if (!root) {
-      root = unique_ptr<Node<K, V>>(new Node<K, V>(key, value));
-      return;
-    }
-    auto ptr = root.get(); // traversing unique_ptr.
-    while (ptr) {
-      if (ptr->key > key) {
-        if (ptr->left) {
-          ptr = ptr->left.get();
-        } else {
-          ptr->left = unique_ptr<Node<K, V>>(new Node<K, V>(key, value));
-          break;
-        }
-      } else if (ptr->key < key) {
-        if (ptr->right) {
-          ptr = ptr->right.get();
-        } else {
-          ptr->right = unique_ptr<Node<K, V>>(new Node<K, V>(key, value));
-          break;
-        }
-      } else {
-        ptr->value = value;
-        break;
-      }
+    unique_ptr<Node<K, V>> &ptr = find(key);
+    if (ptr) {
+      ptr->value = value;
+    } else {
+      ptr.reset(new Node<K, V>(key, value));
     }
   }
+
   bool remove(const K &key) {
-    if (!root) {
+    unique_ptr<Node<K, V>> &ptr = find(key);
+    if (!ptr) {
       return false;
-    }
-    unique_ptr<Node<K, V>> *parentptr = &root;
-    Node<K, V> *ptr = root.get();
-    while (ptr) {
-      if (ptr->key > key) {
-        if (ptr->left) {
-          parentptr = &(ptr->left);
-          ptr = ptr->left.get();
-        } else {
-          return false;
-        }
-      } else if (ptr->key < key) {
-        if (ptr->right) {
-          parentptr = &(ptr->right);
-          ptr = ptr->right.get();
-        } else {
-          return false;
-        }
-      } else {
-        // found the element!
-        if (!ptr->left && ptr->right) {
-          *parentptr = move(ptr->right);
-        } else if (!ptr->right && ptr->left) {
-          *parentptr = move(ptr->left);
-        } else if (!ptr->left && !ptr->right) {
-          parentptr->reset();
-        } else {
-          // contains both :(. remove and append to right.
-          auto right = move(ptr->right);
-          *parentptr = move(ptr->left);
-          Node<K, V>* appendpoint = parentptr->get();
-          while (appendpoint->right) {
-            appendpoint = appendpoint->right.get();
-          }
-          (appendpoint)->right = move(right);
-        }
-        return true;
+    } else if (!ptr->left && ptr->right) {
+      ptr = move(ptr->right);
+    } else if (!ptr->right && ptr->left) {
+      ptr = move(ptr->left);
+    } else if (!ptr->left && !ptr->right) {
+      ptr.reset();
+    } else {
+      // contains both :(. remove and append to right.
+      auto right = move(ptr->right);
+      ptr = move(ptr->left);
+      Node<K, V> *appendptr = ptr.get();
+      while (appendptr->right) {
+        appendptr = appendptr->right.get();
       }
+      appendptr->right = move(right);
     }
-    return false;
+    return true;
   }
+
   bool contains(const K &key) const {
-    if (!root) {
-      return false;
-    }
-    auto ptr = root.get(); // traversing unique_ptr.
-    while (ptr) {
-      if (ptr->key > key) {
-        if (ptr->left) {
-          ptr = ptr->left.get();
-        } else {
-          return false;
-        }
-      } else if (ptr->key < key) {
-        if (ptr->right) {
-          ptr = ptr->right.get();
-        } else {
-          return false;
-        }
-      } else {
-        return true;
-      }
-    }
-    return false;
+    return find(key) ? true : false;
   }
+
   const V &get(const K &key) {
-    if (!root) {
-      return EMPTY;
-    }
-    auto ptr = root.get(); // traversing unique_ptr.
-    while (ptr) {
-      if (ptr->key > key) {
-        if (ptr->left) {
-          ptr = ptr->left.get();
-        } else {
-          return EMPTY;
-        }
-      } else if (ptr->key < key) {
-        if (ptr->right) {
-          ptr = ptr->right.get();
-        } else {
-          return EMPTY;
-        }
-      } else {
-        return ptr->value;
-      }
-    }
-    return EMPTY;
+    const unique_ptr<Node<K, V>> &ptr = find(key);
+    return ptr ? ptr-> value : EMPTY;
   }
+
   const uint64_t size() const {
     if (!root) {
       return 0;
     }
     return root->size();
   }
+
   const uint64_t depth() const {
     if (!root) {
       return 0;
@@ -163,36 +116,77 @@ public:
   }
 };
 
-int main() {
+bool test1() {
   Tree<int, string> t;
-  string foo = "foo";
+  t.put(1, "b");
+  t.put(0, "a");
+  t.put(2, "c");
+  if (t.size() != 3) {
+    return false;
+  }
+  for (auto i = 0; i < 3; i++) {
+    if (!t.contains(i)) {
+      return false;
+    }
+  }
+  if (t.get(0) != "a") {
+    return false;
+  }
+  if (t.get(1) != "b") {
+    return false;
+  }
+  if (t.get(2) != "c") {
+    return false;
+  }
+  if (t.contains(-1)) {
+    return false;
+  }
+  if (t.contains(4)) {
+    return false;
+  }
+  t.remove(1);
+  t.remove(0);
+  t.remove(2);
+  if (t.size() != 0) {
+    return false;
+  }
+  return true;
+}
+
+bool test2() {
+  auto count = 0;
+  Tree<int, string> t;
   for (auto i = 0; i < 10; i++) {
     for (auto j = 0; j < 10; j++) {
-      t.put(i + j * 10, foo);
+      t.put(i + j * 10, "value");
+      count++;
+    }
+    if (t.size() != count) {
+      return false;
     }
   }
-  cout << "size:  " << t.size() << endl;
-  cout << "depth: " << t.depth() << endl;
-  for (auto i = 0; i < 100; i++) {
-    if (!t.contains(i)) {
-      cout << "does not contain " << i << endl;
+  if (t.size() != 100) {
+    return false;
+  }
+  return true;
+}
+
+bool test3() {
+  auto count = 0;
+  Tree<int, string> t;
+  for (auto i = 0; i < 10; i++) {
+    for (auto j = 0; j < 10; j++) {
+      t.put(j, "value");
     }
-    if (t.get(i) != foo) {
-      cout << "does not contain the correct value " << i << endl;
+    if (t.size() != 10) {
+      return false;
     }
   }
-  for (auto j = 9; j >= 0; j--) {
-    for (auto i = 9; i >= 0; i--) {
-      t.remove(i + j * 10);
-    }
-  }
-  Tree<int, string> t2;
-  t2.put(1, "a");
-  t2.put(0, "b");
-  t2.put(2, "c");
-  t2.remove(1);
-  t2.remove(0);
-  t2.remove(2);
-  cout << t2.size() << endl;
-  cout << t2.depth() << endl;
+  return true;
+}
+
+int main() {
+  cout << test1() << endl;
+  cout << test2() << endl;
+  cout << test3() << endl;
 }
