7c52c0f43a0298b99740238d24a901debce03f09
Jeeyoung Kim <jee@squareup.com>
2016-02-20 21:03:27 -0800

    Convex hull algorithm

--- python/convexhull.py
+++ python/convexhull.py
@@ -10,12 +10,17 @@ def calculate_angle(source, point):
     x1, y1 = point
     dx = x1 - x0
     dy = y1 - y0
-    # length = (dx ** 2 + dy ** 2) ** 0.5
     angle = math.atan2(dy, dx)
     return (angle, point)
 
 def calculate_angles(source, points):
     return [calculate_angle(source, point) for point in points]
+
+def normalize_angle(theta, delta):
+    if theta > delta:
+        return theta - delta
+    else:
+        return theta - delta + math.pi * 2
 
 def convexhull(points):
     """using the gift wrapping method"""
@@ -24,26 +29,26 @@ def convexhull(points):
     while True:
         last = hull[-1]
         angles = calculate_angles(last, points)
-        output = [angle for angle in angles if angle[1] not in hull]
-        if len(hull) > 1:
-            output += [calculate_angle(last, hull[0])]
-        output = sorted(output)
-        next_point = min(output)
-        hull.append(next_point[1])
-        if hull[-1] == hull[0]:
-            break
+        output = [angle for angle in angles if angle[1] not in hull] # list of (angle, points)
+        if len(hull) == 1:
+            next_point = min(output, key=lambda x: x[0])[1]
+        else:
+            second_last = hull[-2]
+            output += [calculate_angle(last, hull[0])] # first hull point is always a candidate.
+            delta = calculate_angle(second_last, last)[0]
+            next_point = min(output, key=lambda x: normalize_angle(x[0], delta))[1]
+        hull.append(next_point)
+        if hull[-1] == hull[0]: break # terminal condition
     return hull
 
 def generate_code(n):
+    """generate path command codes for matplotlib for a convex hull"""
     result = []
-    for i in xrange(n):
-        if i == 0:
-            result.append(Path.MOVETO)
-        elif i == n - 1:
-            result.append(Path.CLOSEPOLY)
-        else:
-            result.append(Path.LINETO)
-    return result
+    def index_to_code(i):
+        if i == 0: return Path.MOVETO
+        elif i == n - 1: return Path.CLOSEPOLY
+        else: return Path.LINETO
+    return map(index_to_code, xrange(n))
 
 def execute(points):
     hull = convexhull(points)
@@ -51,13 +56,14 @@ def execute(points):
     path = Path(hull, codes)
     fig = matplotlib.pyplot.figure()
     ax = fig.add_subplot(111)
+    limit = 10
     patch = matplotlib.patches.PathPatch(path, facecolor='orange', alpha = 0.1, lw=2)
+    # plot the points
     x = [p[0] for p in points]
     y = [p[1] for p in points]
-    print x
     ax.scatter(x, y, s = 10)
+    # plot the convex hull
     ax.add_patch(patch)
-    limit = 10
     ax.set_xlim(-limit, limit)
     ax.set_ylim(-limit, limit)
     matplotlib.pyplot.show()
@@ -68,10 +74,30 @@ def test_1():
 def test_2():
     execute([(1, 0), (-1, 0), (0, 1), (0, -1), (0, 0), (0, -0.25)])
 
-def test_random():
+def test_random_1():
     result = []
-    for i in range(10):
+    for i in range(100):
         result.append((numpy.random.normal(), numpy.random.normal()))
     execute(result)
 
-test_2()
+def test_random_2():
+    """two gaussian distributions"""
+    result = []
+    for i in range(1000):
+        result.append((2 + numpy.random.normal(), 2 + numpy.random.normal()))
+    for i in range(1000):
+        result.append((-2 + numpy.random.normal(), -2 + numpy.random.normal()))
+    execute(result)
+
+def test_random_3():
+    """three gaussian distributions"""
+    result = []
+    for i in range(100):
+        result.append((3 + numpy.random.normal(), numpy.random.normal()))
+    for i in range(100):
+        result.append((-3 + numpy.random.normal(), numpy.random.normal()))
+    for i in range(100):
+        result.append((numpy.random.normal(), -3 + numpy.random.normal()))
+    execute(result)
+
+test_random_3()
