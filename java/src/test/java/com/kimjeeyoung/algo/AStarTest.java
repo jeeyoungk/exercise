e3fb5b096e3328210c7307d769a6a88a60278017
Jeeyoung Kim <jee@squareup.com>
2016-03-11 12:15:03 -0800

    Removed System.err.printLn

--- java/src/test/java/com/kimjeeyoung/algo/AStarTest.java
+++ java/src/test/java/com/kimjeeyoung/algo/AStarTest.java
@@ -1,56 +1,60 @@
 package com.kimjeeyoung.algo;
 
+import org.junit.After;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Created by jeeyoungk on 3/10/16.
- */
 public class AStarTest {
+  private static final boolean PRINT_BOARD = false;
+  private Board board;
+  private AStar astar;
+
+  @After
+  public void printBoard() {
+    if (PRINT_BOARD) {
+      System.err.println(astar);
+    }
+  }
 
   @Test
   public void testExecuteLongBoard() throws Exception {
-    Board board = new Board(10, 20);
-    AStar astar = new AStar(new AStar.Coordinate(0, 0), new AStar.Coordinate(19, 9), board);
+    board = new Board(10, 20);
+    astar = new AStar(new AStar.Coordinate(0, 0), new AStar.Coordinate(19, 9), board);
     assertEquals(28, astar.execute());
     assertEquals(164, astar.shortestDistance.size());
-    System.err.println(astar.toString());
   }
 
   @Test
   public void testExecuteSingleStep() throws Exception {
-    Board board = new Board(10, 10);
-    AStar astar = new AStar(new AStar.Coordinate(0, 0), new AStar.Coordinate(1, 1), board);
+    board = new Board(10, 10);
+    astar = new AStar(new AStar.Coordinate(0, 0), new AStar.Coordinate(1, 1), board);
     assertEquals(2, astar.execute());
     assertEquals(5, astar.shortestDistance.size());
-    System.err.println(astar.toString());
   }
 
   @Test
   public void testExecuteObstacle() throws Exception {
-    Board board = new Board(3, 3);
+    board = new Board(3, 3);
     board.board[0][1] = true;
     board.board[1][1] = true;
-    AStar astar = new AStar(new AStar.Coordinate(0, 0), new AStar.Coordinate(0, 2), board);
+    astar = new AStar(new AStar.Coordinate(0, 0), new AStar.Coordinate(0, 2), board);
     assertEquals(6, astar.execute());
     assertEquals(7, astar.shortestDistance.size());
-    System.err.println(astar.toString());
   }
 
   @Test
   public void testPath() throws Exception {
-    Board board = new Board(21, 21);
+    board = new Board(21, 21);
     board.board[10][10] = true;
-    AStar astar = new AStar(new AStar.Coordinate(10, 2), new AStar.Coordinate(10, 18), board);
+    astar = new AStar(new AStar.Coordinate(10, 2), new AStar.Coordinate(10, 18), board);
     assertEquals(18, astar.execute());
     assertEquals(86, astar.shortestDistance.size());
-    System.err.println(astar.toString());
   }
 
   @Test
   public void testWall() throws Exception {
-    Board board = new Board(21, 21);
+    board = new Board(21, 21);
     for (int i = 3; i < 21; i++) {
       board.board[i][10] = true;
     }
@@ -60,19 +64,17 @@ public class AStarTest {
     for (int i = 3; i < 21; i++) {
       board.board[i][14] = true;
     }
-    AStar astar = new AStar(new AStar.Coordinate(10, 2), new AStar.Coordinate(10, 18), board);
+    astar = new AStar(new AStar.Coordinate(10, 2), new AStar.Coordinate(10, 18), board);
     assertEquals(64, astar.execute());
-    System.out.println(astar.toString());
   }
 
   @Test
   public void testBlocked() throws Exception {
-    Board board = new Board(21, 21);
+    board = new Board(21, 21);
     for (int i = 0; i < 21; i++) {
       board.board[i][10] = true;
     }
-    AStar astar = new AStar(new AStar.Coordinate(10, 2), new AStar.Coordinate(10, 18), board);
+    astar = new AStar(new AStar.Coordinate(10, 2), new AStar.Coordinate(10, 18), board);
     assertEquals(-1, astar.execute());
-    System.err.println(astar.toString());
   }
 }
