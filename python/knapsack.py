fc86acdd1caa981b61eed036914016a1fc51a6fa
Jeeyoung Kim <jee@squareup.com>
2016-05-01 11:56:12 -0700

    Added knapsack problem solution

---
+++ python/knapsack.py
@@ -0,0 +1,30 @@
+# integer Knapsack problem implementation
+
+def knapsack(size, inputs):
+    inputs = sorted(inputs)
+    history = {0: ()}
+    for cur_input in inputs:
+        # taking a copy, as we're modifying the dictionary
+        # as we are iterating.
+        for prev_value, prev_history in history.items():
+            new_value = prev_value + cur_input
+            new_history = prev_history + (cur_input,)
+            if new_value == size: return new_history
+            history[new_value] = new_history
+    return None
+
+def knapsack_wrapper(size, inputs):
+    result = knapsack(size, inputs)
+    if result is None:
+        print "%d is not possible from combining %s" % (size, ", ".join(map(str, inputs)))
+    else:
+        print "%d = %s" % (size, " + ".join(map(str, result)))
+
+knapsack_wrapper(10, [10])
+knapsack_wrapper(10, [3, 5, 2])
+knapsack_wrapper(2536, [132,524,241,523,251,231,634])
+knapsack_wrapper(10, [1,2,3,4,5])
+knapsack_wrapper(63, [10, 20, 30, 32, 21])
+knapsack_wrapper(10, [3, 8])
+knapsack_wrapper(10, [1])
+knapsack_wrapper(2535, [132,524,241,523,251,231,634])
