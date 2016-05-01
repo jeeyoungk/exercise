849b30c1a71616aef9bf9a42f94aaedc2941b6bf
Jeeyoung Kim <jee@squareup.com>
2016-05-01 12:00:30 -0700

    formatted the code a bit

--- python/knapsack.py
+++ python/knapsack.py
@@ -4,21 +4,17 @@ def knapsack(size, inputs):
     inputs = sorted(inputs)
     history = {0: ()}
     for cur_input in inputs:
-        # taking a copy, as we're modifying the dictionary
-        # as we are iterating.
-        for prev_value, prev_history in history.items():
+        for prev_value, prev_history in history.items(): # items instead of iteritems, to take a deep copy
             new_value = prev_value + cur_input
             new_history = prev_history + (cur_input,)
             if new_value == size: return new_history
             history[new_value] = new_history
-    return None
+    return None # failed to  find a sum.
 
 def knapsack_wrapper(size, inputs):
     result = knapsack(size, inputs)
-    if result is None:
-        print "%d is not possible from combining %s" % (size, ", ".join(map(str, inputs)))
-    else:
-        print "%d = %s" % (size, " + ".join(map(str, result)))
+    if result is None: print "%d is not possible from combining %s" % (size, ", ".join(map(str, inputs)))
+    else: print "%d = %s" % (size, " + ".join(map(str, result)))
 
 knapsack_wrapper(10, [10])
 knapsack_wrapper(10, [3, 5, 2])
