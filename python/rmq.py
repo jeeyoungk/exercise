d3307e27eac61df60d5c5d490dffba050c769ece
Jeeyoung Kim <jee@squareup.com>
2016-08-17 20:48:39 -0700

    Added RMQ

--- python/rmq.py
+++ python/rmq.py
@@ -3,6 +3,9 @@
 import math
 import random
 
+def log2(v):
+    return int(math.log(v, 2))
+
 def naive_rmq(array):
     """
     prep - O(1)
@@ -15,13 +18,41 @@ def naive_rmq(array):
         return v
     return inner
 
-def pow_2_rmq(array):
+def rmq_by_block(array):
+    N = len(array)
+    block_width = log2(N)
+    num_blocks = N / block_width
+    if num_blocks * block_width < N: num_blocks += 1
+    min_blocks = [None] * num_blocks
+    for i in xrange(N):
+        block_idx = i / block_width
+        if min_blocks[block_idx] is None or min_blocks[block_idx] > array[i]:
+            min_blocks[block_idx] = array[i]
+    
+    inner_rmq = rmq_pow_2(min_blocks)
+
+    def inner(start, end):
+        start_block = start / block_width
+        end_block = end / block_width
+        if start_block == end_block:
+            return min(array[start:end + 1])
+        # O(log(N))
+        start_min = min(array[start:(start_block + 1) * block_width])
+        end_min = min(array[end_block * block_width:end + 1])
+        if start_block + 1 <= end_block - 1:
+            # O(1)
+            block_min = inner_rmq(start_block + 1, end_block - 1)
+            return min(start_min, block_min, end_min)
+        return min(start_min, end_min)
+    return inner
+
+def rmq_pow_2(array):
     """
     prep - O(n log n)
     exec - O(1)
     """
     N = len(array)
-    P = int(math.floor(math.log(N, 2)))
+    P = log2(N)
     width = 1
     last_row = array
     mins = [array]
@@ -34,35 +65,38 @@ def pow_2_rmq(array):
         last_row = new_mins
         width *= 2 
     def inner(start, end):
-        if start == end:
-            return array[start]
+        if start == end: return array[start]
         delta = end - start
-        length = int(math.floor(math.log(delta, 2)))
-        start_index = start
+        length = log2(delta)
         end_index = end - (2 ** length - 1)
         cur_mins = mins[length]
-        return min(cur_mins[start_index], cur_mins[end_index])
+        return min(cur_mins[start], cur_mins[end_index])
     return inner
 
-def rmq_tester(rmq_maker):
-    N = 16
+def rmq_tester_1(rmq_maker, N):
     # test 1 - sequential
     rmq = rmq_maker(range(N))
     for i in xrange(N):
         for j in xrange(i, N):
             assert rmq(i, j) == min(i, j)
+
+def rmq_tester_2(rmq_maker, N):
     # test 2 - reverse sequential
     rmq = rmq_maker(list(reversed(range(N))))
     for i in xrange(N):
         for j in xrange(i, N):
             assert rmq(i, j) == N - max(i, j) - 1
+
+def rmq_tester_3(rmq_maker, N):
     # test 3 - 1 minimum
     test = [1] * N
     test[10] = 0
     rmq = rmq_maker(test)
-    for i in range(N):
-        for j in range(i, N):
+    for i in xrange(N):
+        for j in xrange(i, N):
             assert rmq(i, j) == (0 if (i <= 10 <= j) else 1)
+
+def rmq_tester_4(rmq_maker, N):
     # test 4 - random test
     test = range(N)
     random.shuffle(test)
@@ -71,4 +105,14 @@ def rmq_tester(rmq_maker):
     for i in xrange(N):
         for j in xrange(i, N):
             assert rmq(i, j) == canonical(i, j)
-rmq_tester(pow_2_rmq)
+
+def rmq_tester(rmq_maker):
+    for N in (16, 32, 64):
+        rmq_tester_1(rmq_maker, N)
+        rmq_tester_2(rmq_maker, N)
+        rmq_tester_3(rmq_maker, N)
+        rmq_tester_4(rmq_maker, N)
+
+rmq_tester(naive_rmq)
+rmq_tester(rmq_pow_2)
+rmq_tester(rmq_by_block)
