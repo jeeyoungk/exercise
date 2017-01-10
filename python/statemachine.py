ae1ce31a20c14496f3cfabd8373e7bb4fbfbb522
Jeeyoung Kim <jee@squareup.com>
2017-01-09 17:03:54 -0800

    state machine transition

--- python/statemachine.py
+++ python/statemachine.py
@@ -7,7 +7,9 @@ class StateMachine(object):
         'error_state',
         'states',
         'transitions',
+        # states
         'state',
+        'version',
     )
 
     def __init__(self, start_state, end_state, error_state, states, transitions):
@@ -22,16 +24,17 @@ class StateMachine(object):
         for t in transitions:
             if t.from_state not in self.transitions:
                 self.transitions[t.from_state] = []
-            trs = self.transitions[t.from_state]
-            trs.append(t)
+            self.transitions[t.from_state].append(t)
 
         self.state = self.start_state
+        self.version = 0
 
     def accept_value(self, input_value):
         trs = self.transitions[self.state]
         for tr in trs:
             if input_value in tr.values:
                 self.state = tr.to_state
+                self.version += 1
                 return
         self.state = self.error_state
 
@@ -48,25 +51,40 @@ class Transition(object):
     def __repr__(self):
         return "%s%s" % (type(self).__name__, (self.from_state, self.to_state, self.values))
 
-def transition(json_object):
-    return Transition(
-        json_object['from'],
-        json_object['to'],
-        json_object['values'],
-    )
+    @classmethod
+    def from_json(cls, json_object):
+        return cls(
+            json_object['from'],
+            json_object['to'],
+            json_object['values'],
+        )
+
+class Log(object):
+    __slots__ = ('from_state', 'to_state', 'value', 'version')
 
 def main():
     input_object = json.load(open('statemachine.json', 'rb'))
-    transitions = [transition(x) for x in input_object['transitions']]
+    transitions = [Transition.from_json(x) for x in input_object['transitions']]
     sm = StateMachine(
         input_object['start'],
         input_object['end'],
         input_object['error'],
         states = input_object['states'],
-        transitions=transitions
+        transitions = transitions
     )
     assert sm.state == 'start'
-    sm.accept_value('xyz')
+    sm.accept_value('begin')
+    assert sm.version == 1
+    assert sm.state == 'middle'
+    sm.accept_value('process')
+    sm.accept_value('process')
+    sm.accept_value('process')
+    sm.accept_value('process')
+    sm.accept_value('process')
+    assert sm.version == 6
+    assert sm.state == 'middle'
+    sm.accept_value('finish')
+    assert sm.version == 7
     assert sm.state == 'end'
 
 if __name__ == '__main__':
