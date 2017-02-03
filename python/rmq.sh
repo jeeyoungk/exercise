642dda5599847db7712f5b93352ea35f61ec93dc
Jeeyoung Kim <jee@squareup.com>
2017-02-03 01:50:40 -0800

    update the test script

--- python/rmq.sh
+++ python/rmq.sh
@@ -5,7 +5,12 @@ time pypy rmq.py --algorithm pow2      --run 1 --size $SIZE
 time pypy rmq.py --algorithm subblocks --run 1 --size $SIZE
 time pypy rmq.py --algorithm blocks    --run 1 --size $SIZE
 
-echo "Runs"
+echo "Runs - pypy"
 time pypy rmq.py --algorithm pow2      --run 1000000 --size $SIZE
 time pypy rmq.py --algorithm subblocks --run 1000000 --size $SIZE
 time pypy rmq.py --algorithm blocks    --run 1000000 --size $SIZE
+
+echo "Runs - cpython"
+time python2 rmq.py --algorithm pow2      --run 1000000 --size $SIZE
+time python2 rmq.py --algorithm subblocks --run 1000000 --size $SIZE
+time python2 rmq.py --algorithm blocks    --run 1000000 --size $SIZE
